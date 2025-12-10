60bf7279d74cd4098f393f6be8f9e1ddbed5d9e3
Florian Hansen <flhansen.93@gmail.com>
2025-12-10 23:48:04 +0100

    feat(jobs): add sql queries and migrations for jobs

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem(system: 
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        go-migrate = pkgs.go-migrate.overrideAttrs(oldAttrs: {
+            tags = ["postgres"];
+        });
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            go-migrate
+            sqlc
+          ];
+        };
+      });
+}
